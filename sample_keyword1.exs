128f78ea04af9e722b00eabd551107e89aca5783
Yasuhiro Okada <yasuhiro.okada@aktsk.jp>
2015-11-13 10:52:47 +0900

    keyword

---
+++ sample_keyword1.exs
@@ -0,0 +1,12 @@
+defmodule Main do
+  def match([{:kind, kind} | _]) do
+    IO.puts "match : #{inspect(kind)}"
+  end
+
+  def match(_) do
+    IO.puts "default"
+  end
+end
+
+Main.match [kind: :Field, arguments: ["a", "b", "c"], extra: "extra"]
+Main.match %{kind: :Field}
---
+++ sample_keyword2.exs
@@ -0,0 +1,13 @@
+defmodule Main do
+  def match([{:kind, _}, {:arguments, _} | _ = optionals]) do
+    IO.puts "match : #{inspect(optionals)}"
+  end
+
+  def match(_) do
+    IO.puts "default"
+  end
+end
+
+Main.match [kind: :Field, arguments: ["a", "b", "c"], extra: "extra", extra2: "extra"]
+Main.match [kind: :Field, arguments: ["a", "b", "c"]]
+Main.match %{kind: :Field}
